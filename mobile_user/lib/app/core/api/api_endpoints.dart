2a2875aa2284ee08f4a19ea4e0bb49f8425d9163
Nasywa Kamila <153457446+NasywaKml@users.noreply.github.com>
2026-06-10 11:53:06 +0700

    added endpoint

--- mobile_user/lib/app/core/api/api_endpoints.dart
+++ mobile_user/lib/app/core/api/api_endpoints.dart
@@ -4,5 +4,6 @@ class ApiEndpoints {
 
   static const profile = '/user/profile';
   static const experience = '/user/profile/experience';
+  
   static const updatePassword = '/user/profile/password';
 }
